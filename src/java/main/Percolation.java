0eaa98bf5e62156b93c7011eb9871bfcaa407bc8
Marcin A. Nowak <ian@sonyx.net>
2018-09-05 22:35:11 +0200

    submited and works

--- src/java/main/Percolation.java
+++ src/java/main/Percolation.java
@@ -1,91 +1,99 @@
-import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    WeightedQuickUnionUF weightedQuickUnionUF = null;
-    private int size = 0;
-    private int numberOfOpenSites = 0;
-    private boolean openMarker[];
-    private int topElementIndex;
-    private int bottomElementIndex;
+  private final WeightedQuickUnionUF weightedQuickUnionUf;
+  private final int size;
+  private final int topElementIndex;
+  private final int bottomElementIndex;
+  private int numberOfOpenSites = 0;
+  private boolean[] openMarker;
 
-    public Percolation(int n)                // create n-by-n grid, with all sites blocked
-    {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.size = n;
-        final int gridSize = this.size * this.size;
-        this.openMarker = new boolean[gridSize];
+  /**
+   * constructor.
+   *
+   * @param n size
+   */
+  public Percolation(int n) {
+    if (n <= 0) {
+      throw new IllegalArgumentException();
+    }
+    this.size = n;
+    final int gridSize = this.size * this.size;
+    this.openMarker = new boolean[gridSize];
 
-        this.weightedQuickUnionUF = new WeightedQuickUnionUF(gridSize + 2);
-        this.topElementIndex = gridSize;
-        this.bottomElementIndex = gridSize + 1;
+    this.weightedQuickUnionUf = new WeightedQuickUnionUF(gridSize + 2);
+    this.topElementIndex = gridSize;
+    this.bottomElementIndex = gridSize + 1;
+  }
+
+  public void open(int row, int col) {
+    if (row < 1 || row > this.size || col < 1 || col > this.size) {
+      throw new IllegalArgumentException();
     }
 
-    public void open(int row, int col) {
-        if (row < 1 || row > this.size || col < 1 || col > this.size) {
-            throw new IllegalArgumentException();
-        }
+    int elementIndex = getElementIndex(row, col);
 
-        int elementIndex = getElementIndex(row, col);
+    if (!openMarker[elementIndex]) {
+      final int top = elementIndex - this.size;
+      final int bottom = elementIndex + this.size;
+      final int left = elementIndex - 1;
+      final int right = elementIndex + 1;
 
-        if (openMarker[elementIndex] == false) {
-            final int top = elementIndex - this.size;
-            final int bottom = elementIndex + this.size;
-            final int left = elementIndex - 1;
-            final int right = elementIndex + 1;
+      if (col > 1 && isOpen(row, col - 1)) {
+        this.weightedQuickUnionUf.union(elementIndex, left);
+      }
+      if (col < this.size && isOpen(row, col + 1)) {
+        this.weightedQuickUnionUf.union(elementIndex, right);
+      }
 
-            if (col > 1 && isOpen(row, col - 1)) {
-                this.weightedQuickUnionUF.union(elementIndex, left);
-            }
-            if (col < this.size && isOpen(row, col + 1)) {
-                this.weightedQuickUnionUF.union(elementIndex, right);
-            }
+      if (row == 1) {
+        this.weightedQuickUnionUf.union(elementIndex, topElementIndex);
+      }
+      if (row > 1 && isOpen(row - 1, col)) {
+        this.weightedQuickUnionUf.union(elementIndex, top);
+      }
+      if (row == this.size) {
+        this.weightedQuickUnionUf.union(elementIndex, bottomElementIndex);
+      }
+      if (row < this.size) {
+        this.weightedQuickUnionUf.union(elementIndex, bottom);
+      }
 
-            if (row == 1) {
-                this.weightedQuickUnionUF.union(elementIndex, topElementIndex);
-            }
-            if (row > 1 && isOpen(row - 1, col)) {
-                this.weightedQuickUnionUF.union(elementIndex, top);
-            }
-            if (row == this.size) {
-                this.weightedQuickUnionUF.union(elementIndex, bottomElementIndex);
-            }
-            if (row < this.size)
-                this.weightedQuickUnionUF.union(elementIndex, bottom);
-
-            this.openMarker[elementIndex] = true;
-            this.numberOfOpenSites++;
-        }
-
-    }    // open site (row, col) if it is not open already
-
-    private int getElementIndex(int row, int col) {
-        return (row - 1) * this.size + col - 1;
+      this.openMarker[elementIndex] = true;
+      this.numberOfOpenSites++;
     }
 
-    public boolean isOpen(int row, int col) {
-        if (row < 1 || row > this.size || col < 1 || col > this.size) {
-            throw new IllegalArgumentException();
-        }
-        return openMarker[getElementIndex(row, col)];
-    }  // is site (row, col) open?
+  }    // open site (row, col) if it is not open already
 
-    public boolean isFull(int row, int col) {
-        if (row < 1 || row > this.size || col < 1 || col > this.size) {
-            throw new IllegalArgumentException();
-        }
-        return !isOpen(row, col);
-    }  // is site (row, col) full?
+  private int getElementIndex(int row, int col) {
+    return (row - 1) * this.size + col - 1;
+  }
 
-    public int numberOfOpenSites() {
-        return this.numberOfOpenSites;
-    }       // number of open sites
+  public boolean isOpen(int row, int col) {
+    if (row < 1 || row > this.size || col < 1 || col > this.size) {
+      throw new IllegalArgumentException();
+    }
+    return openMarker[getElementIndex(row, col)];
+  }  // is site (row, col) open?
 
-    public boolean percolates() {
-        return this.weightedQuickUnionUF.find(topElementIndex) == this.weightedQuickUnionUF.find(bottomElementIndex);
-    }              // does the system percolate?
+  public boolean isFull(int row, int col) {
+    if (row < 1 || row > this.size || col < 1 || col > this.size) {
+      throw new IllegalArgumentException();
+    }
+    return
+      isOpen(row, col) &&
+        find(getElementIndex(row, col)) == find(topElementIndex);
+  }  // is site (row, col) full?
 
+  public int numberOfOpenSites() {
+    return this.numberOfOpenSites;
+  }       // number of open sites
+
+  public boolean percolates() {
+    return find(topElementIndex) == find(bottomElementIndex);
+  }              // does the system percolate?
+
+  private int find(int n) {
+    return this.weightedQuickUnionUf.find(n);
+  }
 }
--- src/java/main/PercolationStats.java
+++ src/java/main/PercolationStats.java
@@ -1,54 +1,54 @@
-import edu.princeton.cs.algs4.RandomSeq;
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class PercolationStats {
-    int size;
-    private double thresholds[];
+  private static final double PERCENT_95_CONFIDENCE = 1.96d;
+  private final int size;
+  private final double[] thresholds;
+  private final double mean;
 
-    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
-    {
-        if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException();
-        }
-        this.size = n;
-        this.thresholds = new double[trials];
-        for (int i = 0; i < trials; i++) {
-            final Percolation percolation = new Percolation(n);
-            thresholds[i] = 0;
-
-            while (!percolation.percolates()) {
-                final int col = StdRandom.uniform(n) + 1;
-                final int row = StdRandom.uniform(n) + 1;
-                percolation.open(row, col);
-            }
-            thresholds[i] = ((double) percolation.numberOfOpenSites()) / (size * size);
-        }
+  public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
+  {
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException();
     }
+    this.size = n;
+    this.thresholds = new double[trials];
+    for (int i = 0; i < trials; i++) {
+      final Percolation percolation = new Percolation(n);
+      thresholds[i] = 0;
 
-    public static void main(String[] args)        // test client (described below)
-    {
-
+      while (!percolation.percolates()) {
+        final int col = StdRandom.uniform(n) + 1;
+        final int row = StdRandom.uniform(n) + 1;
+        percolation.open(row, col);
+      }
+      thresholds[i] = ((double) percolation.numberOfOpenSites()) / (size * size);
     }
+    this.mean = StdStats.mean(this.thresholds);
+  }
 
-    public double mean()                          // sample mean of percolation threshold
-    {
-        return StdStats.mean(this.thresholds);
-    }
+  public static void main(String[] args) {
+    return;
+  }
 
-    public double stddev()                        // sample standard deviation of percolation threshold
-    {
-        return StdStats.stddev(this.thresholds);
-    }
+  public double mean()                          // sample mean of percolation threshold
+  {
+    return this.mean;
+  }
 
-    public double confidenceLo()                  // low  endpoint of 95% confidence interval
-    {
-        return (mean() - 1.96 * stddev() / this.size);
-    }
+  public double stddev()                        // sample standard deviation of percolation threshold
+  {
+    return StdStats.stddev(this.thresholds);
+  }
 
-    public double confidenceHi()                  // high endpoint of 95% confidence interval
-    {
-        return (mean() + 1.96 * stddev() / this.size);
-    }
+  public double confidenceLo()                  // low  endpoint of 95% confidence interval
+  {
+    return (this.mean - PERCENT_95_CONFIDENCE * stddev() / this.size);
+  }
+
+  public double confidenceHi()                  // high endpoint of 95% confidence interval
+  {
+    return (this.mean + PERCENT_95_CONFIDENCE * stddev() / this.size);
+  }
 }
